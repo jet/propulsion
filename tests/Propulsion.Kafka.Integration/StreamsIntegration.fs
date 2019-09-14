1dcdeca7c917a8529afb51aef0abbd4c643e76f0
Ruben Bartelink <ruben@bartelink.com>
2019-09-14 18:00:28 +0100

    Add T1Stream

--- tests/Propulsion.Kafka.Integration/StreamsIntegration.fs
+++ tests/Propulsion.Kafka.Integration/StreamsIntegration.fs
@@ -117,7 +117,7 @@ type private T1Batch(testOutputHelper) =
     override __.RunConsumers(log, config, numConsumers, consumerCallback) : Async<unit> =
         runConsumersBatch log config numConsumers None consumerCallback
 
-and private T1Stream(testOutputHelper) =
+and T1Stream(testOutputHelper) =
     inherit T1(testOutputHelper)
 
     override __.RunConsumers(log, config, numConsumers, consumerCallback) : Async<unit> =
