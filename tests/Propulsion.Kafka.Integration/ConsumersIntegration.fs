737dd3e43ac6c85e89511679e6660fc21bbe8503
Ruben Bartelink <ruben@bartelink.com>
2020-07-27 22:44:23 +0100

    Reintroduce parallelism in test suite (#76)

--- tests/Propulsion.Kafka.Integration/ConsumersIntegration.fs
+++ tests/Propulsion.Kafka.Integration/ConsumersIntegration.fs
@@ -262,12 +262,7 @@ and [<AbstractClass>] ConsumerIntegration(testOutputHelper, expectConcurrentSche
     [<FactIfBroker>]
     member __.``producer-consumer basic roundtrip`` () = async {
         let numProducers = 10
-#if DEBUG
         let numConsumers = 10
-#else
-        // TODO debug why this is happy locally but not on the AzureDevOps CI Rig
-        let numConsumers = 1
-#endif
         let messagesPerProducer = 1000
 
         let topic = newId() // dev kafka topics are created and truncated automatically
