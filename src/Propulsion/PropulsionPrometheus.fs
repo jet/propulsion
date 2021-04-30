500fa6d858dfcb757f6dbfc0899533d29507c39e
Asti <asti@dynz.net>
2021-04-30 18:01:44 +0530

    Replace fixed app tag with custom tags (#109)

--- src/Propulsion/PropulsionPrometheus.fs
+++ src/Propulsion/PropulsionPrometheus.fs
@@ -1,80 +1,114 @@
+// This file implements a Serilog Sink `LogSink` that publishes metric values to Prometheus.
+
 namespace Propulsion.Prometheus
 
+[<AutoOpen>]
 module private Impl =
 
     let baseName stat = "propulsion_scheduler_" + stat
     let baseDesc desc = "Propulsion Scheduler " + desc
-    let groupLabels = [| "app"; "group"; "state" |]
-    let activityLabels = [| "app"; "group"; "activity" |]
-    let latencyLabels = [| "app"; "group"; "kind" |]
+    let groupLabels = [| "group"; "state" |]
+    let activityLabels = [| "group"; "activity" |]
+    let latencyLabels = [| "group"; "kind" |]
+
+    let [<Literal>] secondsStat = "_seconds"
+    let [<Literal>] latencyDesc = " latency"
+
+    let append = Array.append
 
 module private Gauge =
 
-    let private mk (cfg : Prometheus.GaugeConfiguration) name help =
-        let g = Prometheus.Metrics.CreateGauge(name, help, cfg)
-        fun app group state v -> g.WithLabels(app, group, state).Set(v)
-    let private config = Prometheus.GaugeConfiguration(LabelNames = Impl.groupLabels)
-    let create stat desc = mk config (Impl.baseName stat) (Impl.baseDesc desc)
+    let private make (config : Prometheus.GaugeConfiguration) name desc =
+        let gauge = Prometheus.Metrics.CreateGauge(name, desc, config)
+        fun tagValues group state value ->
+            let labelValues = append tagValues [| group; state; |]
+            gauge.WithLabels(labelValues).Set(value)
+
+    
+    let create (tagNames, tagValues) stat desc =
+        let config = Prometheus.GaugeConfiguration(LabelNames = append tagNames groupLabels)
+        make config (Impl.baseName stat) (Impl.baseDesc desc) tagValues
 
 module private Counter =
 
-    let private mk (cfg : Prometheus.CounterConfiguration) name desc =
-        let c = Prometheus.Metrics.CreateCounter(name, desc, cfg)
-        fun app group activity v -> c.WithLabels(app, group, activity).Inc(v)
-    let private config = Prometheus.CounterConfiguration(LabelNames = Impl.activityLabels)
-    let create stat desc =
-        mk config (Impl.baseName stat) (Impl.baseDesc desc)
+    let private make (config : Prometheus.CounterConfiguration) name desc =
+        let counter = Prometheus.Metrics.CreateCounter(name, desc, config)
+        fun tagValues group activity value ->
+            let labelValues = append tagValues [| group; activity; |]
+            counter.WithLabels(labelValues).Inc(value)
+    
+    let create (tagNames, tagValues) stat desc =
+        let config = Prometheus.CounterConfiguration(LabelNames = append tagNames activityLabels)
+        make config (Impl.baseName stat) (Impl.baseDesc desc) tagValues
 
 module private Summary =
 
-    let private create (cfg : Prometheus.SummaryConfiguration) name desc  =
-        let s = Prometheus.Metrics.CreateSummary(name, desc, cfg)
-        fun app (group, kind) v -> s.WithLabels(app, group, kind).Observe(v)
-    let config =
-        let inline qep q e = Prometheus.QuantileEpsilonPair(q, e)
-        let objectives = [| qep 0.50 0.05; qep 0.95 0.01; qep 0.99 0.01 |]
-        Prometheus.SummaryConfiguration(Objectives = objectives, LabelNames = Impl.latencyLabels, MaxAge = System.TimeSpan.FromMinutes 1.)
-    let latency stat desc = create config (Impl.baseName stat + "_seconds") (Impl.baseDesc desc + " latency")
+    let private create (config : Prometheus.SummaryConfiguration) name desc  =
+        let summary = Prometheus.Metrics.CreateSummary(name, desc, config)
+        fun tagValues (group, kind) value ->
+            let labelValues = append tagValues [| group; kind; |]
+            summary.WithLabels(labelValues).Observe(value)
+
+    let private objectives =
+           [|
+               0.50, 0.05 // Between 45th and 55th percentile
+               0.95, 0.01 // Between 94th and 96th percentile
+               0.99, 0.01 // Between 100th and 98th percentile
+           |] |> Array.map Prometheus.QuantileEpsilonPair
+
+    let latency (tagNames, tagValues) stat desc =
+        let config =
+            let labelValues = append tagNames Impl.latencyLabels
+            Prometheus.SummaryConfiguration(Objectives = objectives, LabelNames = labelValues, MaxAge = System.TimeSpan.FromMinutes 1.)
+
+        create config (Impl.baseName stat + secondsStat) (Impl.baseDesc desc + latencyDesc) tagValues
 
 module private Histogram =
 
-    let private create (cfg : Prometheus.HistogramConfiguration) name desc =
-        let h = Prometheus.Metrics.CreateHistogram(name, desc, cfg)
-        fun app (group, kind) v -> h.WithLabels(app, group, kind).Observe(v)
-    let private sHistogram =
-        let sBuckets = Prometheus.Histogram.ExponentialBuckets(0.001, 2., 16) // 1ms .. 64s
-        let sCfg = Prometheus.HistogramConfiguration(Buckets = sBuckets, LabelNames = Impl.latencyLabels)
-        create sCfg
-    let latency stat desc = sHistogram (Impl.baseName stat + "_seconds") (Impl.baseDesc desc + " latency")
+    let private create (config : Prometheus.HistogramConfiguration) name desc =
+        let histogram = Prometheus.Metrics.CreateHistogram(name, desc, config)
+        fun tagValues (group, kind) value ->
+            let labelValues = append tagValues [| group; kind; |]
+            histogram.WithLabels(labelValues).Observe(value)
+
+    let private sBuckets =
+        Prometheus.Histogram.ExponentialBuckets(0.001, 2., 16) // 1ms .. 64s
+
+    let latency  (tagNames, tagValues) stat desc =        
+        let config = Prometheus.HistogramConfiguration(Buckets = sBuckets, LabelNames = append tagNames Impl.latencyLabels)
+        create config (Impl.baseName stat + secondsStat) (Impl.baseDesc desc + latencyDesc) tagValues
 
 open Propulsion.Streams.Log
 
-module private Stats =
+/// <summary>An ILogEventSink that publishes to Prometheus</summary>
+/// <param name="customTags">Custom tags to annotate the metric we're publishing where such tag manipulation cannot better be achieved via the Prometheus scraper config.</param>
+/// <param name="group">ChangeFeedProcessor consumer group name.</param>
+type LogSink(customTags: seq<string * string>, group: string) =
 
-    let observeCats =    Gauge.create      "cats"            "Current categories"
-    let observeStreams = Gauge.create      "streams"         "Current streams"
-    let observeEvents =  Gauge.create      "events"          "Current events"
-    let observeBytes =   Gauge.create      "bytes"           "Current bytes"
+    let tags = Array.ofSeq customTags |> Array.unzip
+    
+    let observeCats =    Gauge.create      tags "cats"            "Current categories"
+    let observeStreams = Gauge.create      tags "streams"         "Current streams"
+    let observeEvents =  Gauge.create      tags "events"          "Current events"
+    let observeBytes =   Gauge.create      tags "bytes"           "Current bytes"
+                                           
+    let observeCpu =     Counter.create    tags "cpu"             "Processing Time Breakdown"
+                                           
+    let observeLatSum =  Summary.latency   tags "handler_summary" "Handler action"
+    let observeLatHis =  Histogram.latency tags "handler"         "Handler action"
 
-    let observeCpu =     Counter.create    "cpu"             "Processing Time Breakdown"
+    let observeState ctx state (m : BufferMetric) =
+        observeCats ctx state (float m.cats)
+        observeStreams ctx state (float m.streams)
+        observeEvents ctx state (float m.events)
+        observeBytes ctx state (float m.bytes)
 
-    let observeLatSum =  Summary.latency   "handler_summary" "Handler action"
-    let observeLatHis =  Histogram.latency "handler"         "Handler action"
-
-    let observeState app ctx state (m : BufferMetric) =
-        observeCats app ctx state (float m.cats)
-        observeStreams app ctx state (float m.streams)
-        observeEvents app ctx state (float m.events)
-        observeBytes app ctx state (float m.bytes)
-
-type LogSink(app, group) =
-
-    let observeState = Stats.observeState app group
-    let observeCpu = Stats.observeCpu app group
+    let observeState = observeState group
+    let observeCpu = observeCpu group
     let observeLatency kind latenciesS =
         for v in latenciesS do
-            Stats.observeLatSum app (group, kind) v
-            Stats.observeLatHis app (group, kind) v
+           observeLatSum (group, kind) v
+           observeLatHis (group, kind) v
 
     interface Serilog.Core.ILogEventSink with
         member __.Emit logEvent = logEvent |> function
@@ -82,7 +116,7 @@ type LogSink(app, group) =
                 | Metric.BufferReport m ->
                     observeState "ingesting" m
                 | Metric.SchedulerStateReport (synced, busyStats, readyStats, bufferingStats, malformedStats) ->
-                    Stats.observeStreams app group "synced" (float synced)
+                    observeStreams group "synced" (float synced)
                     observeState "active" busyStats
                     observeState "ready" readyStats
                     observeState "buffering" bufferingStats
