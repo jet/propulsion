632c0ad2b0d3ce9cc4f4c70f8824e702fe55bb2e
Ruben Bartelink <ruben@bartelink.com>
2022-04-12 10:48:34 +0100

    Add Index read logic

--- src/Propulsion.DynamoDb/Types.fs
+++ src/Propulsion.DynamoDb/Types.fs
@@ -30,3 +30,41 @@ module AppendsEpochId =
 /// Identifies an Equinox Store Stream; used within an AppendsEpoch
 type IndexStreamId = int<indexStreamId>
 and [<Measure>] indexStreamId
+
+module internal Config =
+
+    open Equinox.DynamoStore
+    let createDecider stream = Equinox.Decider(Serilog.Log.Logger, stream, maxAttempts = 3)
+
+    let private createCached codec initial fold accessStrategy (context, cache) =
+        let cacheStrategy = CachingStrategy.SlidingWindow (cache, System.TimeSpan.FromMinutes 20.)
+        DynamoStoreCategory(context, codec, fold, initial, cacheStrategy, accessStrategy)
+
+    let createSnapshotted codec initial fold (isOrigin, toSnapshot) (context, cache) =
+        let accessStrategy = AccessStrategy.Snapshot (isOrigin, toSnapshot)
+        createCached codec initial fold accessStrategy (context, cache)
+
+    let createUnoptimized codec initial fold (context, cache) =
+        let accessStrategy = AccessStrategy.Unoptimized
+        createCached codec initial fold accessStrategy (context, cache)
+
+    let private createUncached codec initial fold accessStrategy context =
+        let cacheStrategy = CachingStrategy.NoCaching
+        DynamoStoreCategory(context, codec, fold, initial, cacheStrategy, accessStrategy)
+
+    let createUncachedUnoptimized codec initial fold context =
+        let accessStrategy = AccessStrategy.Unoptimized
+        createUncached codec initial fold accessStrategy context
+
+module internal EventCodec =
+
+    open FsCodec.SystemTextJson
+
+    let create<'t when 't :> TypeShape.UnionContract.IUnionContract> () =
+        Codec.Create<'t>().ToByteArrayCodec()
+    let private withUpconverter<'c, 'e when 'c :> TypeShape.UnionContract.IUnionContract> up : FsCodec.IEventCodec<'e, _, _> =
+        let down (_ : 'e) = failwith "Unexpected"
+        Codec.Create<'e, 'c, _>(up, down).ToByteArrayCodec()
+    let withIndex<'c when 'c :> TypeShape.UnionContract.IUnionContract> : FsCodec.IEventCodec<int64 * 'c, _, _> =
+        let up (raw : FsCodec.ITimelineEvent<_>, e) = raw.Index, e
+        withUpconverter<'c, int64 * 'c> up
