ac8ffc2df753a0b5dfedf90e68b87c0b2a669d5d
Ruben Bartelink <ruben@bartelink.com>
2022-04-20 09:38:39 +0100

    DyanmoStore.Lambda base wiring

---
+++ src/Propulsion.DynamoStore.Lambda/Function.fs
@@ -0,0 +1,20 @@
+﻿namespace Propulsion.DynamoStore.Lambda
+
+open Amazon.Lambda.Core
+open Amazon.Lambda.DynamoDBEvents
+open Propulsion.DynamoStore
+
+[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
+()
+
+type Function() =
+
+    let cfg = Configuration()
+    let conn = Connector(cfg)
+    let cache = Equinox.Cache("indexer", sizeMb = 10)
+    let service = DynamoStoreIndexer.Config.create 100_000 (conn.Context, cache)
+
+    member _.FunctionHandler(dynamoEvent : DynamoDBEvent, context : ILambdaContext) = task {
+        do! DynamoStreamsLambda.ingest service dynamoEvent context
+        context.Logger.LogInformation("Stream processing complete.")
+    }
--- src/Propulsion.DynamoStore/DynamoStreamsLambda.fs
+++ src/Propulsion.DynamoStore/DynamoStreamsLambda.fs
@@ -1,37 +1,32 @@
 module Propulsion.DynamoStore.DynamoStreamsLambda
 
-
 open Amazon.DynamoDBv2
 open Amazon.Lambda.Core
 open Amazon.Lambda.DynamoDBEvents
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
-()
+let ingest (service : DynamoStoreIndexer.Service) (dynamoEvent : DynamoDBEvent) (context : ILambdaContext) : Async<unit> =
 
-type Function() =
+    let spans = ResizeArray<AppendsEpoch.Events.StreamSpan>()
 
-    member _.Handler(dynamoEvent : DynamoDBEvent, context : ILambdaContext) =
+    let processRecord (record: DynamoDBEvent.DynamodbStreamRecord) =
+        context.Logger.LogInformation(sprintf "Event ID: %s" record.EventID)
+        context.Logger.LogInformation(sprintf "Event Name: %s" record.EventName.Value)
 
+        match record.Dynamodb.StreamViewType with
+        | x when x = StreamViewType.NEW_IMAGE || x = StreamViewType.NEW_AND_OLD_IMAGES -> ()
+        | x -> invalidOp (sprintf "Unexpected StreamViewType %O" x)
 
-        let processRecord (record: DynamoDBEvent.DynamodbStreamRecord) =
-            context.Logger.LogInformation(sprintf "Event ID: %s" record.EventID)
-            context.Logger.LogInformation(sprintf "Event Name: %s" record.EventName.Value)
-            context.Logger.LogInformation(sprintf "Event Name: %s" record.Dynamodb.StreamViewType.Value) // keys only is fine
-            match record.Dynamodb.StreamViewType with
-            | x when x = StreamViewType.NEW_IMAGE || x = StreamViewType.NEW_AND_OLD_IMAGES -> ()
-            | x -> invalidOp (sprintf "Unexpected StreamViewType %O" x)
+        let sn, i = IndexStreamId.ofP record.Dynamodb.Keys["p"].S, int record.Dynamodb.Keys["i"].N
+        let appendedLen = int record.Dynamodb.NewImage["a"].N
+        let appendedSpanEventTypes =
+            let batchEventTypes = record.Dynamodb.NewImage["c"].NS
+            batchEventTypes.GetRange(batchEventTypes.Count - appendedLen, appendedLen).ToArray()
 
-            let sn, i = IndexStreamId.ofP record.Dynamodb.Keys["p"].S, int record.Dynamodb.Keys["i"].N
-            let appendedLen = int record.Dynamodb.NewImage["a"].N
-            let appendedSpanEventTypes =
-                let batchEventTypes = record.Dynamodb.NewImage["c"].NS
-                batchEventTypes.GetRange(batchEventTypes.Count - appendedLen, appendedLen)
+        spans.Add { p = sn; i = i; c = appendedSpanEventTypes }
 
-            context.Logger.LogInformation(sprintf "Span %O @ %d : %A" sn i appendedSpanEventTypes)
-            // TODO: Add business logic processing the record.Dynamodb object.
+        context.Logger.LogInformation(sprintf "Span %O @ %d : %A" sn i appendedSpanEventTypes)
 
-        dynamoEvent.Records
-        |> Seq.iter processRecord
+    dynamoEvent.Records
+    |> Seq.iter processRecord
 
-        context.Logger.LogInformation("Stream processing complete.")
+    service.Ingest(AppendsTrancheId.wellKnownId, spans.ToArray())
