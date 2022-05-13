46b6c71140982f78f846260382fad8d890655fc4
Ruben Bartelink <ruben@bartelink.com>
2022-05-13 18:47:28 +0100

    Tidy during Remove DynamoStore in #139

--- src/Propulsion.CosmosStore/ReaderCheckpoint.fs
+++ src/Propulsion.CosmosStore/ReaderCheckpoint.fs
@@ -8,7 +8,7 @@ let streamName (source, tranche, consumerGroupName : string) =
         // This form is only used for interop with the V3 Propulsion.Feed.FeedSource - anyone starting with V4 should only ever encounter tripartite names
         FsCodec.StreamName.compose Category [SourceId.toString source; TrancheId.toString tranche]
     else
-        let (*[<Literal>]*) Category = "$ReaderCheckpoint3"
+        let (*[<Literal>]*) Category = "$ReaderCheckpoint3" // TODO replace with 2 for final release
         FsCodec.StreamName.compose Category [SourceId.toString source; TrancheId.toString tranche; consumerGroupName]
 
 // NB - these schemas reflect the actual storage formats and hence need to be versioned with care
