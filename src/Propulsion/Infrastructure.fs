3c11142b75bf3b0ef2181fd106a4b17c0b2313ef
Ruben Bartelink <ruben@bartelink.com>
2022-05-18 11:43:48 +0100

    Sync with canonical AwaitTaskCorrect cf https://github.com/jet/equinox/blob/master/src/Equinox.Core/Infrastructure.fs

--- src/Propulsion/Infrastructure.fs
+++ src/Propulsion/Infrastructure.fs
@@ -4,45 +4,48 @@ module internal Propulsion.Infrastructure
 open System
 open System.Threading.Tasks
 
+// http://www.fssnip.net/7Rc/title/AsyncAwaitTaskCorrect pending that being officially packaged somewhere or integrated into FSharp.Core
 type Async with
+
+    /// <summary>
+    ///     Gets the result of given task so that in the event of exception
+    ///     the actual user exception is raised as opposed to being wrapped
+    ///     in a System.AggregateException.
+    /// </summary>
+    /// <param name="task">Task to be awaited.</param>
+    [<System.Diagnostics.DebuggerStepThrough>]
+    static member AwaitTaskCorrect(task : Task<'T>) : Async<'T> =
+        Async.FromContinuations(fun (sc, ec, _cc) ->
+            task.ContinueWith(fun (t : Task<'T>) ->
+                if t.IsFaulted then
+                    let e = t.Exception
+                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions[0]
+                    else ec e
+                elif t.IsCanceled then ec (TaskCanceledException())
+                else sc t.Result)
+            |> ignore)
+
+    /// <summary>
+    ///     Gets the result of given task so that in the event of exception
+    ///     the actual user exception is raised as opposed to being wrapped
+    ///     in a System.AggregateException.
+    /// </summary>
+    /// <param name="task">Task to be awaited.</param>
+    [<System.Diagnostics.DebuggerStepThrough>]
+    static member AwaitTaskCorrect(task : Task) : Async<unit> =
+        Async.FromContinuations(fun (sc, ec, _cc) ->
+            task.ContinueWith(fun (task : Task) ->
+                if task.IsFaulted then
+                    let e = task.Exception
+                    if e.InnerExceptions.Count = 1 then ec e.InnerExceptions[0]
+                    else ec e
+                elif task.IsCanceled then
+                    ec (TaskCanceledException())
+                else
+                    sc ())
+            |> ignore)
 
     static member Sleep(t : TimeSpan) : Async<unit> = Async.Sleep(int t.TotalMilliseconds)
 
     /// Re-raise an exception so that the current stacktrace is preserved
     static member Raise(e : #exn) : Async<'T> = Async.FromContinuations (fun (_,ec,_) -> ec e)
-
-    /// <summary>
-    ///     Gets the result of given task so that in the event of exception
-    ///     the actual user exception is raised as opposed to being wrapped
-    ///     in a System.AggregateException.
-    /// </summary>
-    /// <param name="task">Task to be awaited.</param>
-    static member AwaitTaskCorrect (task : Task<'T>) : Async<'T> =
-        Async.FromContinuations <| fun (k, ek, _) ->
-            task.ContinueWith (fun (t : Task<'T>) ->
-                if t.IsFaulted then
-                    let e = t.Exception
-                    if e.InnerExceptions.Count = 1 then ek e.InnerExceptions.[0]
-                    else ek e
-                elif t.IsCanceled then ek (TaskCanceledException("Task wrapped with Async has been cancelled."))
-                elif t.IsCompleted then k t.Result
-                else ek(Exception "invalid Task state!"))
-            |> ignore
-
-    /// <summary>
-    ///     Gets the result of given task so that in the event of exception
-    ///     the actual user exception is raised as opposed to being wrapped
-    ///     in a System.AggregateException.
-    /// </summary>
-    /// <param name="task">Task to be awaited.</param>
-    static member AwaitTaskCorrect (task : Task) : Async<unit> =
-        Async.FromContinuations <| fun (k, ek, _) ->
-            task.ContinueWith (fun (t : Task) ->
-                if t.IsFaulted then
-                    let e = t.Exception
-                    if e.InnerExceptions.Count = 1 then ek e.InnerExceptions.[0]
-                    else ek e
-                elif t.IsCanceled then ek (TaskCanceledException("Task wrapped with Async has been cancelled."))
-                elif t.IsCompleted then k ()
-                else ek(Exception "invalid Task state!"))
-            |> ignore
