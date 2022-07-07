09d9ab2155e04aabd7553327231910025b5c5f90
Ruben Bartelink <ruben@bartelink.com>
2022-07-07 15:45:30 +0100

    More comments on CDK logic

--- src/Propulsion.DynamoStore.Cdk/PropulsionDynamoStoreStack.fs
+++ src/Propulsion.DynamoStore.Cdk/PropulsionDynamoStoreStack.fs
@@ -10,7 +10,6 @@ type PropulsionDynamoStoreStack(scope, id, props, ?fromTail) as stack =
     let streamArn = CfnParameter(stack, "streamsArn", CfnParameterProps(
         Type = "String",
         Description = "DynamoDB Streams ARN for source"))
-    // let table = Amazon.CDK.AWS.DynamoDB.Table.FromTableAttributes(stack, "et", TableAttributes(TableArn = tableArn))
 
     let role =
         let role = Role(stack, "DynamoStoreIndexerLambda", RoleProps(
@@ -18,6 +17,7 @@ type PropulsionDynamoStoreStack(scope, id, props, ?fromTail) as stack =
             // Basic required permissions, chiefly CloudWatch access
             ManagedPolicies = [| ManagedPolicy.FromAwsManagedPolicyName "service-role/AWSLambdaBasicExecutionRole" |]))
         // Enable access to DDB Streams Top Level Streams List
+        // TODO figure out of this is even necessary ?
         do  let topLevel = PolicyStatement()
             topLevel.AddActions("dynamodb:ListStreams")
             topLevel.AddAllResources()
@@ -29,7 +29,8 @@ type PropulsionDynamoStoreStack(scope, id, props, ?fromTail) as stack =
             role.AddToPolicy(streamLevel) |> ignore
         role
 
-    //  dotnet publish -c Release -r linux-arm64 --self-contained false
+    // NOTE: see README.md; must be built before using this via
+    //       dotnet publish ../Propulsion.DynamoStore.Lambda -c Release -r linux-arm64 --self-contained true
     let code = Code.FromAsset("../Propulsion.DynamoStore.Lambda/bin/Release/net6.0/linux-arm64/publish/")
     let fn : Function = Function(stack, "PropulsionDynamoStoreLambda", FunctionProps(
         Role = role,
@@ -40,4 +41,7 @@ type PropulsionDynamoStoreStack(scope, id, props, ?fromTail) as stack =
         EventSourceArn = streamArn.ValueAsString,
         StartingPosition = (if fromTail = Some true then StartingPosition.LATEST else StartingPosition.TRIM_HORIZON),
         // >1000 has proven not to work well in 128 MB memory
+        // Anything up to 9999 is viable and tends to provide the best packing density and throughput under load. The key cons are:
+        // - granularity of checkpointing is increased (so killing a projector may result in more re-processing than it would with a lower batch size)
+        // - while aggregate write (and read) costs will go down, individual requests will be larger which may affect latency and/or required RC provisioning
         BatchSize = 1000.)) |> ignore
