f438e525998ced6a15e764c3c2b4e9be2ad3ddf6
Ruben Bartelink <ruben@bartelink.com>
2022-08-03 11:48:44 +0100

    Reinstate workaround for FSharp.Core 6.0.4 bug
    Equivalent symptoms still happening in 6.0.5

--- src/Propulsion.DynamoStore/Types.fs
+++ src/Propulsion.DynamoStore/Types.fs
@@ -86,7 +86,30 @@ module internal EventCodec =
 
 module internal Async =
 
-    let parallelThrottled dop computations =
-        // NOTE this requires FSharp.Core 6.0.5 as there can be > 1200 computations and v <= 6.0.4 had a tail recursion bug
-        // https://github.com/dotnet/fsharp/issues/13165
+    open Propulsion.Infrastructure // AwaitTaskCorrect
+
+    type Async with
+        static member Throttle degreeOfParallelism =
+            let s = new System.Threading.SemaphoreSlim(degreeOfParallelism)
+            fun computation -> async {
+                let! ct = Async.CancellationToken
+                do! s.WaitAsync ct |> Async.AwaitTaskCorrect
+                try return! computation
+                finally s.Release() |> ignore }
+    let private parallelThrottledUnsafe dop computations = // https://github.com/dotnet/fsharp/issues/13165
         Async.Parallel(computations, maxDegreeOfParallelism = dop)
+    // NB this should not be necessary if targeting FSharp.Core 6.0.5 as we do
+    // However my eyes say otherwise so this stays until someone figures out what the problem is, i.e.
+    // - if we are getting the wrong version
+    // - the patch did not make it into 6.0.5
+    // - or there is some other secondary issue in play
+    let parallelThrottled dop computations = async {
+        let throttle = Async.Throttle dop // each batch of 1200 gets the full potential dop - we internally limit what actually gets to run concurrently here
+        let! allResults =
+            computations
+            |> Seq.map throttle
+            |> Seq.chunkBySize 1200
+            |> Seq.map (parallelThrottledUnsafe dop)
+            |> Async.Parallel
+        return Array.concat allResults
+    }
