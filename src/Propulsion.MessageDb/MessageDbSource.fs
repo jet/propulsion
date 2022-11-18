171e2a20bbcd00aea958927c72d0b434129f0cd0
Einar Norðfjörð <einar@nordfjord.io>
2022-11-18 11:46:35 -0500

    move the client into a core module

--- src/Propulsion.MessageDb/MessageDbSource.fs
+++ src/Propulsion.MessageDb/MessageDbSource.fs
@@ -12,51 +12,53 @@ open System.Data.Common
 open System.Diagnostics
 
 
-type MessageDbCategoryClient(connectionString) =
-    let connect = Npgsql.connect connectionString
-    let parseRow (reader: DbDataReader) =
-        let readNullableString idx = if reader.IsDBNull(idx) then None else Some (reader.GetString idx)
-        let streamName = reader.GetString(8)
-        let event = TimelineEvent.Create(
-            index = reader.GetInt64(0),
-            eventType = reader.GetString(1),
-            data = ReadOnlyMemory(Text.Encoding.UTF8.GetBytes(reader.GetString 2)),
-            meta = ReadOnlyMemory(Text.Encoding.UTF8.GetBytes(reader.GetString 3)),
-            eventId = reader.GetGuid(4),
-            ?correlationId = readNullableString 5,
-            ?causationId = readNullableString 6,
-            context = reader.GetInt64(9),
-            timestamp = DateTimeOffset(DateTime.SpecifyKind(reader.GetDateTime(7), DateTimeKind.Utc)))
+module Core =
+    type MessageDbCategoryClient(connectionString) =
+        let connect = Npgsql.connect connectionString
+        let parseRow (reader: DbDataReader) =
+            let readNullableString idx = if reader.IsDBNull(idx) then None else Some (reader.GetString idx)
+            let streamName = reader.GetString(8)
+            let event = TimelineEvent.Create(
+                index = reader.GetInt64(0),
+                eventType = reader.GetString(1),
+                data = ReadOnlyMemory(Text.Encoding.UTF8.GetBytes(reader.GetString 2)),
+                meta = ReadOnlyMemory(Text.Encoding.UTF8.GetBytes(reader.GetString 3)),
+                eventId = reader.GetGuid(4),
+                ?correlationId = readNullableString 5,
+                ?causationId = readNullableString 6,
+                context = reader.GetInt64(9),
+                timestamp = DateTimeOffset(DateTime.SpecifyKind(reader.GetDateTime(7), DateTimeKind.Utc)))
 
-        struct(StreamName.parse streamName, event)
-    member _.ReadCategoryMessages(category: TrancheId, fromPositionInclusive: int64, batchSize: int, ct) = task {
-        use! conn = connect ct
-        let command = conn.CreateCommand(CommandText = "select position, type, data, metadata, id::uuid,
-                                                               (metadata::jsonb->>'$correlationId')::text,
-                                                               (metadata::jsonb->>'$causationId')::text,
-                                                               time, stream_name, global_position
-                                                        from get_category_messages(@Category, @Position, @BatchSize);")
-        command.Parameters.AddWithValue("Category", NpgsqlDbType.Text, TrancheId.toString category) |> ignore
-        command.Parameters.AddWithValue("Position", NpgsqlDbType.Bigint, fromPositionInclusive) |> ignore
-        command.Parameters.AddWithValue("BatchSize", NpgsqlDbType.Bigint, int64 batchSize) |> ignore
+            struct(StreamName.parse streamName, event)
+        member _.ReadCategoryMessages(category: TrancheId, fromPositionInclusive: int64, batchSize: int, ct) = task {
+            use! conn = connect ct
+            let command = conn.CreateCommand(CommandText = "select position, type, data, metadata, id::uuid,
+                                                                   (metadata::jsonb->>'$correlationId')::text,
+                                                                   (metadata::jsonb->>'$causationId')::text,
+                                                                   time, stream_name, global_position
+                                                            from get_category_messages(@Category, @Position, @BatchSize);")
+            command.Parameters.AddWithValue("Category", NpgsqlDbType.Text, TrancheId.toString category) |> ignore
+            command.Parameters.AddWithValue("Position", NpgsqlDbType.Bigint, fromPositionInclusive) |> ignore
+            command.Parameters.AddWithValue("BatchSize", NpgsqlDbType.Bigint, int64 batchSize) |> ignore
 
-        let mutable checkpoint = fromPositionInclusive
+            let mutable checkpoint = fromPositionInclusive
 
-        use! reader = command.ExecuteReaderAsync(ct)
-        let events = [| while reader.Read() do yield parseRow reader |]
+            use! reader = command.ExecuteReaderAsync(ct)
+            let events = [| while reader.Read() do yield parseRow reader |]
 
-        checkpoint <- match Array.tryLast events with Some (_, ev) -> unbox<int64> ev.Context | None -> checkpoint
+            checkpoint <- match Array.tryLast events with Some (_, ev) -> unbox<int64> ev.Context | None -> checkpoint
 
-        return { checkpoint = Position.parse checkpoint; items = events; isTail = events.Length = 0 } }
-    member _.ReadCategoryLastVersion(category: TrancheId, ct) = task {
-        use! conn = connect ct
-        let command = conn.CreateCommand(CommandText = "select max(global_position) from messages where category(stream_name) = @Category;")
-        command.Parameters.AddWithValue("Category", NpgsqlDbType.Text, TrancheId.toString category) |> ignore
+            return { checkpoint = Position.parse checkpoint; items = events; isTail = events.Length = 0 } }
+        member _.ReadCategoryLastVersion(category: TrancheId, ct) = task {
+            use! conn = connect ct
+            let command = conn.CreateCommand(CommandText = "select max(global_position) from messages where category(stream_name) = @Category;")
+            command.Parameters.AddWithValue("Category", NpgsqlDbType.Text, TrancheId.toString category) |> ignore
 
-        use! reader = command.ExecuteReaderAsync(ct)
-        return if reader.Read() then reader.GetInt64(0) else 0L }
+            use! reader = command.ExecuteReaderAsync(ct)
+            return if reader.Read() then reader.GetInt64(0) else 0L }
 
 module private Impl =
+    open Core
     open Propulsion.Infrastructure // AwaitTaskCorrect
 
     let readBatch batchSize (store : MessageDbCategoryClient) (category, pos) : Async<Propulsion.Feed.Core.Batch<_>> = async {
@@ -72,7 +74,7 @@ module private Impl =
 
 type MessageDbSource
     (   log : Serilog.ILogger, statsInterval,
-        client: MessageDbCategoryClient, batchSize, tailSleepInterval,
+        client: Core.MessageDbCategoryClient, batchSize, tailSleepInterval,
         checkpoints : Propulsion.Feed.IFeedCheckpointStore, sink : Propulsion.Streams.Default.Sink,
         categories,
         // Override default start position to be at the tail of the index. Default: Replay all events.
@@ -89,7 +91,7 @@ type MessageDbSource
                 yield sw.Elapsed, b }),
             string)
     new (log, statsInterval, connectionString, batchSize, tailSleepInterval, checkpoints, sink, trancheIds, ?startFromTail, ?sourceId) =
-        MessageDbSource(log, statsInterval, MessageDbCategoryClient(connectionString),
+        MessageDbSource(log, statsInterval, Core.MessageDbCategoryClient(connectionString),
                         batchSize, tailSleepInterval, checkpoints, sink, trancheIds, ?startFromTail=startFromTail, ?sourceId=sourceId)
 
     abstract member ListTranches : unit -> Async<Propulsion.Feed.TrancheId array>
