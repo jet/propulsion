46aa0941eea6b0890b7dfad5c4272c78a2c8131f
Ruben Bartelink <ruben@bartelink.com>
2022-12-08 11:19:08 +0000

    Tidy

--- src/Propulsion.Feed/FeedReader.fs
+++ src/Propulsion.Feed/FeedReader.fs
@@ -34,63 +34,60 @@ module Log =
         logEvent.Properties.TryGetValue(PropertyTag, &p) |> ignore
         match p with Log.ScalarValue (:? Metric as e) -> ValueSome e | _ -> ValueNone
 
-[<AutoOpen>]
-module private Impl =
+type internal Stats(partition : int, source : SourceId, tranche : TrancheId, renderPos : Position -> string) =
 
-    type Stats(partition : int, source : SourceId, tranche : TrancheId, renderPos : Position -> string) =
+    let mutable batchLastPosition = Position.parse -1L
+    let mutable closed, lastWasTail, finishedReading = false, false, false
 
-        let mutable batchLastPosition = Position.parse -1L
-        let mutable closed, lastWasTail, finishedReading = false, false, false
+    let mutable pagesRead, pagesEmpty, events = 0, 0, 0L
+    let mutable readLatency, recentPagesRead, recentEvents, recentPagesEmpty = TimeSpan.Zero, 0, 0, 0
 
-        let mutable pagesRead, pagesEmpty, events = 0, 0, 0L
-        let mutable readLatency, recentPagesRead, recentEvents, recentPagesEmpty = TimeSpan.Zero, 0, 0, 0
+    let mutable ingestLatency, currentBatches, maxReadAhead = TimeSpan.Zero, 0, 0
 
-        let mutable ingestLatency, currentBatches, maxReadAhead = TimeSpan.Zero, 0, 0
+    let mutable lastCommittedPosition = Position.parse -1L
 
-        let mutable lastCommittedPosition = Position.parse -1L
+    member _.Dump(log : ILogger) =
+        if closed then () else
 
-        member _.Dump(log : ILogger) =
-            if closed then () else
+        let p pos = match pos with p when p = Position.parse -1L -> Nullable() | x -> Nullable x
+        let m = Log.Metric.Read {
+            source = source; tranche = tranche
+            token = p batchLastPosition; latency = readLatency; pages = recentPagesRead; items = recentEvents
+            ingestLatency = ingestLatency; ingestQueued = currentBatches }
+        let readS, postS = readLatency.TotalSeconds, ingestLatency.TotalSeconds
+        let inline r pos = match pos with p when p = Position.parse -1L -> null | x -> renderPos x
+        let state = if not lastWasTail then "Busy"
+                    elif lastCommittedPosition = batchLastPosition then "COMPLETE"
+                    else if finishedReading then "End" else "Tail"
+        (Log.withMetric m log).ForContext("tail", lastWasTail).Information(
+            "Reader {partition} {state} @ {lastCommittedPosition}/{readPosition} Pages {pagesRead} empty {pagesEmpty} events {events} | Recent {l:f1}s Pages {recentPagesRead} empty {recentPagesEmpty} events {recentEvents} | Wait {pausedS:f1}s Ahead {cur}/{max}",
+            partition, state, r lastCommittedPosition, r batchLastPosition, pagesRead, pagesEmpty, events, readS, recentPagesRead, recentPagesEmpty, recentEvents, postS, currentBatches, maxReadAhead)
+        readLatency <- TimeSpan.Zero; ingestLatency <- TimeSpan.Zero
+        recentPagesRead <- 0; recentEvents <- 0; recentPagesEmpty <- 0
+        closed <- finishedReading
 
-            let p pos = match pos with p when p = Position.parse -1L -> Nullable() | x -> Nullable x
-            let m = Log.Metric.Read {
-                source = source; tranche = tranche
-                token = p batchLastPosition; latency = readLatency; pages = recentPagesRead; items = recentEvents
-                ingestLatency = ingestLatency; ingestQueued = currentBatches }
-            let readS, postS = readLatency.TotalSeconds, ingestLatency.TotalSeconds
-            let inline r pos = match pos with p when p = Position.parse -1L -> null | x -> renderPos x
-            let state = if not lastWasTail then "Busy"
-                        elif lastCommittedPosition = batchLastPosition then "COMPLETE"
-                        else if finishedReading then "End" else "Tail"
-            (Log.withMetric m log).ForContext("tail", lastWasTail).Information(
-                "Reader {partition} {state} @ {lastCommittedPosition}/{readPosition} Pages {pagesRead} empty {pagesEmpty} events {events} | Recent {l:f1}s Pages {recentPagesRead} empty {recentPagesEmpty} events {recentEvents} | Wait {pausedS:f1}s Ahead {cur}/{max}",
-                partition, state, r lastCommittedPosition, r batchLastPosition, pagesRead, pagesEmpty, events, readS, recentPagesRead, recentPagesEmpty, recentEvents, postS, currentBatches, maxReadAhead)
-            readLatency <- TimeSpan.Zero; ingestLatency <- TimeSpan.Zero
-            recentPagesRead <- 0; recentEvents <- 0; recentPagesEmpty <- 0
-            closed <- finishedReading
+    member _.RecordBatch(readTime, batch: Batch<_>) =
+        readLatency <- readLatency + readTime
+        batchLastPosition <- batch.checkpoint
+        lastWasTail <- batch.isTail
+        match Array.length batch.items with
+        | 0 ->  pagesEmpty <- pagesEmpty + 1
+                recentPagesEmpty <- recentPagesEmpty + 1
+        | c ->  pagesRead <- pagesRead + 1
+                events <- events + int64 c
+                recentEvents <- recentEvents + c
+                recentPagesRead <- recentPagesRead + 1
 
-        member _.RecordBatch(readTime, batch: Batch<_>) =
-            readLatency <- readLatency + readTime
-            batchLastPosition <- batch.checkpoint
-            lastWasTail <- batch.isTail
-            match Array.length batch.items with
-            | 0 ->  pagesEmpty <- pagesEmpty + 1
-                    recentPagesEmpty <- recentPagesEmpty + 1
-            | c ->  pagesRead <- pagesRead + 1
-                    events <- events + int64 c
-                    recentEvents <- recentEvents + c
-                    recentPagesRead <- recentPagesRead + 1
+    member _.UpdateCommittedPosition(pos) =
+        lastCommittedPosition <- pos
+        closed <- false // Any updates trigger logging
 
-        member _.UpdateCommittedPosition(pos) =
-            lastCommittedPosition <- pos
-            closed <- false // Any updates trigger logging
-
-        member _.UpdateIngesterState(latency, cur, max, ?finished) =
-            ingestLatency <- ingestLatency + latency
-            currentBatches <- cur
-            maxReadAhead <- max
-            finishedReading <- defaultArg finished false
-            closed <- false // Any straggler reads (and/or bugs!) trigger logging
+    member _.UpdateIngesterState(latency, cur, max, ?finished) =
+        ingestLatency <- ingestLatency + latency
+        currentBatches <- cur
+        maxReadAhead <- max
+        finishedReading <- defaultArg finished false
+        closed <- false // Any straggler reads (and/or bugs!) trigger logging
 
 type FeedReader
     (   log : ILogger, partition, source, tranche,
