f7841314b4fd62ab31a812c7cc1d8671382af89b
Nick <nick.coleman@educationperfect.com>
2023-07-21 00:49:05 +1200

    Formatting

--- tests/Propulsion.Tests/AppendsEpochTests.fs
+++ tests/Propulsion.Tests/AppendsEpochTests.fs
@@ -7,7 +7,6 @@ open Xunit
 
 [<Fact>]
 let ``residual span shouldn't be affected by earlier events in closed spans`` () =
-
     let shouldClose = (<) 10
     let spans1: Events.StreamSpan array = [| { p = IndexStreamId.ofP "Cat-Id"; i = 0L; c = [| "a" |] } |]
     let spans2: Events.StreamSpan array = [| { p = IndexStreamId.ofP "Cat-Id"; i = 1L; c = [| "b" |] } |]
@@ -19,9 +18,7 @@ let ``residual span shouldn't be affected by earlier events in closed spans`` ()
     let (_, events2) = Ingest.decide shouldClose spans2 Fold.initial
     let epoch2Open =  Fold.fold Fold.initial events2
 
-    let ({ residual = residual1 }: ExactlyOnceIngester.IngestResult<_,_>,_) =
-        Ingest.decide shouldClose spans3 epoch1Closed
-
+    let ({ residual = residual1 }: ExactlyOnceIngester.IngestResult<_,_>,_) = Ingest.decide shouldClose spans3 epoch1Closed
 
     let ({ accepted = accepted2; residual = residual2 }: ExactlyOnceIngester.IngestResult<_ ,_>, events) =
         Ingest.decide shouldClose residual1 epoch2Open
