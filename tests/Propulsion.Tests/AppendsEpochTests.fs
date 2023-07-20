ebb80c150779a11de00af8b38d4ea16750c1b33f
Nick <nick.coleman@educationperfect.com>
2023-07-21 00:22:53 +1200

    Add failing test

---
+++ tests/Propulsion.Tests/AppendsEpochTests.fs
@@ -0,0 +1,33 @@
+﻿module Propulsion.Tests.AppendsEpochTests
+
+open FSharp.UMX
+open Propulsion.DynamoStore
+open Propulsion.DynamoStore.AppendsEpoch
+open Swensen.Unquote
+open Xunit
+
+[<Fact>]
+let ``residual span shouldn't be affected by earlier events in closed spans`` () =
+
+    let shouldClose = (<) 10
+    let spans1: Events.StreamSpan array = [| { p = IndexStreamId.ofP "Cat-Id"; i = 0L; c = [| "a" |] } |]
+    let spans2: Events.StreamSpan array = [| { p = IndexStreamId.ofP "Cat-Id"; i = 1L; c = [| "b" |] } |]
+    let spans3: Events.StreamSpan array = [| { p = IndexStreamId.ofP "Cat-Id"; i = 2L; c = [| "3" |] } |]
+
+    let (_, events1) = Ingest.decide shouldClose spans1 Fold.initial
+    let epoch1Closed = (Fold.fold Fold.initial events1).WithClosed()
+
+    let (_, events2) = Ingest.decide shouldClose spans2 Fold.initial
+    let epoch2Open =  Fold.fold Fold.initial events2
+
+    let ({ residual = residual1 }: ExactlyOnceIngester.IngestResult<_,_>,_) =
+        Ingest.decide shouldClose spans3 epoch1Closed
+
+
+    let ({ accepted = accepted2; residual = residual2 }: ExactlyOnceIngester.IngestResult<_ ,_>, events) =
+        Ingest.decide shouldClose residual1 epoch2Open
+
+    test <@ residual1 = spans3 && accepted2 = (spans3 |> Array.map (fun {p = p} -> p)) && residual2 = [||] @>
+
+
+
